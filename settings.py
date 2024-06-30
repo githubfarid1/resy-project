abf1933bd6fe2cef67b632754abd1a37180a1024
Mohamad Farid Saleh <github.farid1@gmail.com>
2024-06-30 18:32:26 +0900

    Update Scripts

--- settings.py
+++ settings.py
@@ -6,4 +6,4 @@ RESY_PASSWORD='Upworktest1!'
 HEADLESS='no'
 CLOSE_MESSAGE=" --> Press any key to close..."
 RESERVATION_LIST = ["Table", "Dining Room", "Bar", "Bar Counter", "Kitchen Counter", "Table (Not Bar)", "Side table(not bar)", "Patio", "Street Seating", "WHISKY ROOM", "SUSHI COUNTER", "High Top", "Bar Mutsumi"]
-PERIOD_LIST = ["Dinner", "Lunch"]
+PERIOD_LIST = ["Dinner", "Lunch", "Brunch"]
