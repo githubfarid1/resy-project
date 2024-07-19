c84d9d7e771d277895741777240f6b96dc1598f8
githubfarid1 <88619699+githubfarid1@users.noreply.github.com>
2024-07-19 21:23:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ git_dir = os.getcwd()
 g = git.cmd.Git(git_dir)
 g.pull()		
 # messagebox.showinfo(title='Info', message='the scripts has updated..')
-print("script updated")
+print("script updatedx")
 
 from tkinter import *
 from tkinter import ttk
