55ebddbf88bf1c696ae48525d7cd023ea384cb48
Mohamad Farid Saleh <github.farid1@gmail.com>
2024-07-19 09:49:21 +0900

    update scripts

--- settings.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-import os
-import json
-PYTHON_EXE = os.getcwd() + os.sep + r"venv\Scripts\python.exe"
-CHROME_USER_DATA = os.getcwd() + os.sep + "chrome_profiles"
-CLOSE_MESSAGE=" --> Press any key to close..."
-CONTINUE_MESSAGE=" --> Bot will be restarted..."
-CHROME_EXE = r'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
