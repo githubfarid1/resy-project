77579e1a037a142cce0f21bebba10fd28ff25994
Mohamad Farid Saleh <github.farid1@gmail.com>
2024-08-16 06:28:13 +0900

    update scripts

--- modules/update_token.py
+++ modules/update_token.py
@@ -47,7 +47,7 @@ def intercept_request(request, profilename):
     token = ''
     if "https://api.resy.com/2/config" in request.url:
         print(request.url)
-        # breakpoint()
+        breakpoint()
         try:
             token = request.headers['X-Resy-Auth-Token']
             api_key=str(request.headers['Authorization']).replace('ResyAPI api_key=', "").replace('"','')
