1ad4dca5eabc976cde60732e1872783f36cd729e
Mohamad Farid Saleh <github.farid1@gmail.com>
2024-08-18 12:05:59 +0900

    update scripts

--- runcommand.py
+++ runcommand.py
@@ -5,7 +5,7 @@ if platform == "linux" or platform == "linux2":
 elif platform == "win32":
     pyexe = "venv/Scripts/python.exe"  
 fname = open("logs/command1.log", "w")
-process = Popen([pyexe, "modules/resybotv5b.py", "-u", "https://resy.com/cities/new-york-ny/venues/coqodaq", "-d", "2024-08-19", "-t", "5:00 PM", "-s", "2", "-r", "<Not Set>", "-cp", "resyfarid2@proton.me", "-rd", "2024-08-17", "-rt", "8:51:38 PM", "-rh", "0", "-rn", "Yes", "-ns", "Yes", "-dr", "0", "-up", "new_proxy", "-re", "3", "-mn", "5", "-mx", "10"
+process = Popen([pyexe, "modules/resybotv5b.py", "-u", "https://resy.com/cities/new-york-ny/venues/coqodaq", "-d", "2024-08-19", "-t", "5:00 PM", "-s", "2", "-r", "<Not Set>", "-cp", "resyfarid2@proton.me", "-rd", "2024-08-17", "-rt", "8:51:38 PM", "-rh", "0", "-rn", "Yes", "-ns", "Yes", "-dr", "0", "-up", "<Not Set>", "-re", "3", "-mn", "5", "-mx", "10"
 ], stdout=fname)
 print(process.pid)
 #pid server =  166983
